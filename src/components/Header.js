cc54e220d32e520619e7041f552610725d9b707c
Anis Merchant <anisxmerchant@gmail.com>
2019-06-30 03:34:27 -0400

    implement 'Header' component with accompanying styles

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { PageHeader } from 'antd';
+
+const styles = {
+  pageHeaderContainer: {
+    border: '1px solid #D9D9D9',
+    marginBottom: 10,
+  }
+}
+
+export default () => (
+  <PageHeader 
+    style={styles.pageHeaderContainer} 
+    title="OpenTable" 
+    subTitle="Find restaurants near you" 
+  />
+);
