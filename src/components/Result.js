9fe15c9847674b63d7c4fcc6f028f0d913f89c91
Anis Merchant <anisxmerchant@gmail.com>
2019-06-30 03:35:13 -0400

    implement 'Result' component with accompanying styles

---
+++ src/components/Result.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Card } from 'antd';
+
+const { Meta } = Card;
+
+export default (props) => (     
+    <Card 
+      hoverable
+      style={{ width: 240, margin: 15 }}
+      cover={<img alt="restaurant" src={props.image_url}  />}
+    >
+      <Meta title={props.name} description={props.address} />
+      <p>{props.city}, {props.state} {props.postal_code}</p>
+      <p>${props.price}</p>
+    </Card>
+);
+
