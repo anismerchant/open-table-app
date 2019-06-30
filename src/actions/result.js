da8cbe1e67ce79c003b97f7fb3be66d3f8e9749f
Anis Merchant <anisxmerchant@gmail.com>
2019-06-30 03:15:09 -0400

    implement 'GET_RESTAURANT' action generator coupled with case in reducer

---
+++ src/actions/result.js
@@ -0,0 +1,6 @@
+const getResult = ({ data }) => ({
+  type: 'GET_RESTAURANT',
+  data
+});
+
+export default getResult;
---
+++ src/reducers/result.js
@@ -0,0 +1,12 @@
+const resultDefaultState = {};
+
+const resultReducer = (state=resultDefaultState, action) => {
+  switch(action.type) {
+    case 'GET_RESTAURANT':
+      return action.data
+    default:
+      return state
+  }
+}
+
+export default resultReducer;
